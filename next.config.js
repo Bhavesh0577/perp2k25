f1167300feedad8621286270ffb5caa93c7f243d
Bhavesh0577 <bhaveshshrigiri8@gmail.com>
2025-05-19 13:43:28 +0530

    updates

--- next.config.js
+++ next.config.js
@@ -2,7 +2,9 @@
 const nextConfig = {
   // Enable experimental server actions
   experimental: {
-    serverActions: true,
+    serverActions: {
+      allowedOrigins: ["localhost:3000"]
+    },
   },
   eslint: {
     // Disable ESLint during builds
