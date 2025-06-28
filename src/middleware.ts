2afc1cafbfef6380d6e92d81f90e8cd34dcdb89a
Bhavesh0577 <bhaveshshrigiri8@gmail.com>
2025-06-28 23:50:11 +0530

    new updates

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,6 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
-// Create a matcher for public routes
+
 const publicRoutes = createRouteMatcher([
   '/', 
   '/sign-in(.*)', 
